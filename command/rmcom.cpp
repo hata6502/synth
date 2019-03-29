46213eb637ac79d05219397d5dcee061f9097d46
hato6502 <erisuke12345@yahoo.co.jp>
2019-03-30 00:10:05 +0900

    rmcom コマンドを実装しました。

---
+++ command/rmcom.cpp
@@ -0,0 +1,25 @@
+#include "command.hpp"
+
+#include <core/core.hpp>
+#include <io.hpp>
+
+void rmcomHandler(vector<string> &args) {
+  if (args.size() != 2) {
+    throw runtime_error("構文: rmcom (Component UUID)");
+  }
+
+  uuid_t uuid;
+  if (parseUuid(args[1], &uuid)) {
+    throw runtime_error("不正な Component UUID です。");
+  }
+
+  Component *com = searchCom(uuid);
+  if (!com) {
+    throw runtime_error("存在しない Component です。");
+  }
+
+  g_sketch.removeCom(com);
+
+  EmptyResponse response;
+  respond(response);
+}
--- core/sketch.cpp
+++ core/sketch.cpp
@@ -3,98 +3,86 @@
 #include <algorithm>
 #include <deque>
 
-Sketch::Sketch() : onSim(false)
-{
+Sketch::Sketch() : onSim(false) {}
+
+void Sketch::appendCom(Component *com) {
+  this->coms.push_back(Component_up(com));
 }
 
-void Sketch::appendCom(Component *com)
-{
-	this->coms.push_back(Component_up(com));
+void Sketch::removeCom(Component *rm) {
+  this->coms.erase(
+      remove_if(this->coms.begin(), this->coms.end(),
+                [&](Component_up &com) -> bool { return com.get() == rm; }),
+      this->coms.end());
 }
 
-void Sketch::removeCom(Component_up &rm)
-{
-	this->coms.erase(remove_if(this->coms.begin(), this->coms.end(), [&](Component_up &com) -> bool { return com == rm; }), this->coms.end());
+void Sketch::upInterface() {
+  this->int_ins.clear();
+  this->int_outs.clear();
+  for (Component_up &com : this->coms) {
+    vector<PortIn_p> int_ins = com->getIntIns();
+    this->int_ins.insert(this->int_ins.end(), int_ins.begin(), int_ins.end());
+
+    vector<PortOut_p> int_outs = com->getIntOuts();
+    this->int_outs.insert(this->int_outs.end(), int_outs.begin(),
+                          int_outs.end());
+  }
 }
 
-void Sketch::upInterface()
-{
-	this->int_ins.clear();
-	this->int_outs.clear();
-	for (Component_up &com : this->coms)
-	{
-		vector<PortIn_p> int_ins = com->getIntIns();
-		this->int_ins.insert(this->int_ins.end(), int_ins.begin(), int_ins.end());
+void Sketch::onSimStart() {
+  for (Component_up &com : this->coms) {
+    com->onSimStart();
+  }
 
-		vector<PortOut_p> int_outs = com->getIntOuts();
-		this->int_outs.insert(this->int_outs.end(), int_outs.begin(), int_outs.end());
-	}
+  this->onSim = true;
 }
 
-void Sketch::onSimStart()
-{
-	for (Component_up &com : this->coms)
-	{
-		com->onSimStart();
-	}
+void Sketch::onChangeTime(double dt) {
+  deque<Component *> chcoms;
 
-	this->onSim = true;
+  for (Component_up &com : this->coms) {
+    com->onChangeTime(dt, chcoms);
+  }
+
+  while (!chcoms.empty()) {
+    chcoms.front()->onChangeIn(chcoms);
+    chcoms.pop_front();
+  }
 }
 
-void Sketch::onChangeTime(double dt)
-{
-	deque<Component *> chcoms;
+void Sketch::onSimEnd() { this->onSim = false; }
 
-	for (Component_up &com : this->coms)
-	{
-		com->onChangeTime(dt, chcoms);
-	}
-
-	while (!chcoms.empty())
-	{
-		chcoms.front()->onChangeIn(chcoms);
-		chcoms.pop_front();
-	}
-}
-
-void Sketch::onSimEnd()
-{
-	this->onSim = false;
-}
-
-void Sketch::exportExtends()
-{
-	for (Component_up &com : this->coms)
-	{
-		com->exportExtends();
-	}
+void Sketch::exportExtends() {
+  for (Component_up &com : this->coms) {
+    com->exportExtends();
+  }
 }
 
 /*
-	export(){
-		var ex = {};
-		ex.coms = [];
-		this.coms.forEach((com) => {
-			ex.coms.push(com.export());
-		});
-		return ex;
-	}
+        export(){
+                var ex = {};
+                ex.coms = [];
+                this.coms.forEach((com) => {
+                        ex.coms.push(com.export());
+                });
+                return ex;
+        }
 
-	import(im){
-		this.clearCom();
-		var lut = [];
-		im.coms.forEach((im) => {
-			var com = eval("new "+im.type+"()");
-			lut = lut.concat(com.import(im));
-			this.appendCom(com);
-		});
+        import(im){
+                this.clearCom();
+                var lut = [];
+                im.coms.forEach((im) => {
+                        var com = eval("new "+im.type+"()");
+                        lut = lut.concat(com.import(im));
+                        this.appendCom(com);
+                });
 
-		var comlut = [];
-		this.coms.forEach((com) => {
-			com.connectById(lut);
-			comlut.push({ id: com.id, inst: com });
-		});
-		return comlut;
-	}
+                var comlut = [];
+                this.coms.forEach((com) => {
+                        com.connectById(lut);
+                        comlut.push({ id: com.id, inst: com });
+                });
+                return comlut;
+        }
 };
 */
